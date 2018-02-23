4eb1f8cf59a59f67910ccb8b7d524240f5765373
holishing <holishingtest@gmail.com>
2018-02-23 18:12:00 +0800

    refine sample script

--- sample/proxy2.sh
+++ sample/proxy2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 export GOPATH=$PWD
-go run main.go -r `date +%s` -u wss://term.gamer.com.tw/bbs &
+go run main.go -r `date +%s` -u wss://ws.ptt.cc/bbs &
 socat TCP-LISTEN:$1 UNIX-CONNECT:tmp/telnetBYwebsocket.`date +%s`.sock &
 
