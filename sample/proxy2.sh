853b56c4f6ceb0381c24665c4ac63e1d50363f39
holishing <holishingtest@gmail.com>
2018-03-20 20:49:23 +0800

    add gorilla/websocket autoget script

--- sample/proxy2.sh
+++ sample/proxy2.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 export GOPATH=$PWD
+go get github.com/gorilla/websocket
 go run main.go -r `date +%s` -u wss://ws.ptt.cc/bbs &
 socat TCP-LISTEN:$1 UNIX-CONNECT:tmp/telnetBYwebsocket.`date +%s`.sock &
 
